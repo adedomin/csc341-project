ae9a4d855dd8597ebf2f6ba26f3eb84935d8b244
prussian <genunrest@gmail.com>
2016-12-04 17:46:35 -0500

    fixed invalid insert

--- sql/gym-mock-data/gym-sessions.sql
+++ sql/gym-mock-data/gym-sessions.sql
@@ -1,10 +1,10 @@
-INSERT INTO Team00.Sessions (customer_id, trainer_id, service_fee_id, time, days)
+INSERT INTO Team00.Sessions (customer_id, trainer_id, fee_id, time, days)
 VALUES (34, 1, 2, 1000, 'mwf');
 
-INSERT INTO Team00.Sessions (customer_id, trainer_id, service_fee_id, time, days)
+INSERT INTO Team00.Sessions (customer_id, trainer_id, fee_id, time, days)
 VALUES (30, 3, 2, 1500, 'rf');
 
-INSERT INTO Team00.Sessions (customer_id, trainer_id, service_fee_id, time, days)
+INSERT INTO Team00.Sessions (customer_id, trainer_id, fee_id, time, days)
 VALUES (54, 2, 2, 1300, 'tr');
 
 INSERT INTO Team00.CustomerFees (fee_id, customer_id)
