ce4f3af07dd40ee0d6b39196f2db731f8c3ec369
prussian <genunrest@gmail.com>
2016-12-04 17:45:12 -0500

    no semi

--- sql/gym-mock-data/gym-fees.sql
+++ sql/gym-mock-data/gym-fees.sql
@@ -1,8 +1,8 @@
 INSERT INTO Team00.Fees (fee_amount, fee_desc)
-VALUES (40.00, 'General Membership')
+VALUES (40.00, 'General Membership');
 
 INSERT INTO Team00.Fees (fee_amount, fee_desc)
-VALUES (200.00, 'Personal Training')
+VALUES (200.00, 'Personal Training');
 
 INSERT INTO Team00.Fees (fee_amount, fee_desc)
-VALUES (-25.00, 'Birthday Discount')
+VALUES (-25.00, 'Birthday Discount');
--- sql/gym-mock-data/gym-sessions.sql
+++ sql/gym-mock-data/gym-sessions.sql
@@ -1,18 +1,17 @@
 INSERT INTO Team00.Sessions (customer_id, trainer_id, service_fee_id, time, days)
-VALUES (34, 1, 2, 1000, 'mwf')
+VALUES (34, 1, 2, 1000, 'mwf');
 
 INSERT INTO Team00.Sessions (customer_id, trainer_id, service_fee_id, time, days)
-VALUES (30, 3, 2, 1500, 'rf')
+VALUES (30, 3, 2, 1500, 'rf');
 
 INSERT INTO Team00.Sessions (customer_id, trainer_id, service_fee_id, time, days)
-VALUES (54, 2, 2, 1300, 'tr')
-
-
-INSERT INTO Team00.CustomerFees (fee_id, customer_id)
-VALUES (2, 54)
+VALUES (54, 2, 2, 1300, 'tr');
 
 INSERT INTO Team00.CustomerFees (fee_id, customer_id)
-VALUES (2, 34)
+VALUES (2, 54);
 
 INSERT INTO Team00.CustomerFees (fee_id, customer_id)
-VALUES (2, 30)
+VALUES (2, 34);
+
+INSERT INTO Team00.CustomerFees (fee_id, customer_id)
+VALUES (2, 30);
