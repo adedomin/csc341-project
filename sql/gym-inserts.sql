b8996c7c14674c8c4b6279e4f8daa3334b6ac11b
prussian <genunrest@gmail.com>
2016-12-04 20:24:09 -0500

    fixed maint inserts

--- sql/gym-inserts.sql
+++ sql/gym-inserts.sql
@@ -497,10 +497,10 @@ INSERT INTO Team00.Maintenance (equip_id, employee_id, needs_work, work_date)
 VALUES (4, 7, 'resistor in motor', '6-Dec-2016');
 
 INSERT INTO Team00.Maintenance (equip_id, employee_id, needs_work, work_date)
-VALUES (6, 16, 'belt lubrication', '5-Dec-2016');
+VALUES (6, 10, 'belt lubrication', '5-Dec-2016');
 
 INSERT INTO Team00.Maintenance (equip_id, employee_id, needs_work, work_date)
-VALUES (9, 30, 'check screw tightness', '8-Dec-2016');
+VALUES (9, 5, 'check screw tightness', '8-Dec-2016');
 INSERT INTO Team00.Sessions (customer_id, trainer_id, fee_id, time, days)
 VALUES (34, 1, 2, 1000, 'mwf');
 
--- sql/gym-mock-data/gym-maintenance.sql
+++ sql/gym-mock-data/gym-maintenance.sql
@@ -2,7 +2,7 @@ INSERT INTO Team00.Maintenance (equip_id, employee_id, needs_work, work_date)
 VALUES (4, 7, 'resistor in motor', '6-Dec-2016');
 
 INSERT INTO Team00.Maintenance (equip_id, employee_id, needs_work, work_date)
-VALUES (6, 16, 'belt lubrication', '5-Dec-2016');
+VALUES (6, 10, 'belt lubrication', '5-Dec-2016');
 
 INSERT INTO Team00.Maintenance (equip_id, employee_id, needs_work, work_date)
-VALUES (9, 30, 'check screw tightness', '8-Dec-2016');
+VALUES (9, 5, 'check screw tightness', '8-Dec-2016');
