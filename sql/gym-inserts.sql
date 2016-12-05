6874c5cc85583ee1f27cb362e57a7de9df6f5125
prussian <genunrest@gmail.com>
2016-12-04 20:21:51 -0500

    rewrite insert

--- sql/gym-inserts.sql
+++ sql/gym-inserts.sql
@@ -494,14 +494,13 @@ FROM Team00.Equipment
 WHERE equip_type = 'W'
   AND name LIKE '%plate%';
 INSERT INTO Team00.Maintenance (equip_id, employee_id, needs_work, work_date)
-VALUES (4, 7, 'resistor in motor', '6-Dec-2016')
+VALUES (4, 7, 'resistor in motor', '6-Dec-2016');
 
 INSERT INTO Team00.Maintenance (equip_id, employee_id, needs_work, work_date)
-VALUES (6, 16, 'belt lubrication', '5-Dec-2016')
+VALUES (6, 16, 'belt lubrication', '5-Dec-2016');
 
 INSERT INTO Team00.Maintenance (equip_id, employee_id, needs_work, work_date)
 VALUES (9, 30, 'check screw tightness', '8-Dec-2016');
-
 INSERT INTO Team00.Sessions (customer_id, trainer_id, fee_id, time, days)
 VALUES (34, 1, 2, 1000, 'mwf');
 
